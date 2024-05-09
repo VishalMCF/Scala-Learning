888df62463224258e39363c62090394c4fd8976e
Vishal M Yadav <vishal.yadav@embibe.com>
2024-05-09 16:00:38 +0530

    learned methods and the way they are inherited in scala

--- src/main/scala/scalamethods/MethodDemo.scala
+++ src/main/scala/scalamethods/MethodDemo.scala
@@ -8,16 +8,18 @@ object MethodDemo extends App {
   }
 
   // with return type
-  def isBetweenA(a:Int, x:Int, y:Int): Boolean = a >= x && a <= y
-  def maxNumberA(a:Int, b:Int): Int = if (a > b) a else b
+  def isBetweenA(a: Int, x: Int, y: Int): Boolean = a >= x && a <= y
+
+  def maxNumberA(a: Int, b: Int): Int = if (a > b) a else b
 
   // without return type
-  def isBetweenB(a:Int, x:Int, y:Int) = a >= x && a <= y
-  def maxNumberB(a:Int, b:Int)= if (a > b) a else b
+  def isBetweenB(a: Int, x: Int, y: Int) = a >= x && a <= y
+
+  def maxNumberB(a: Int, b: Int) = if (a > b) a else b
 
   println(addTwoIntegers(4, 5))
-  println(isBetweenA(3,4,5))
-  println(isBetweenB(3,4,5))
-  println(maxNumberB(3,4))
-  println(maxNumberA(3,4))
+  println(isBetweenA(3, 4, 5))
+  println(isBetweenB(3, 4, 5))
+  println(maxNumberB(3, 4))
+  println(maxNumberA(3, 4))
 }
