c8db1afa7f8f89f31778af87b800f6057b95c9ee
Vishal M Yadav <vishal.yadav@embibe.com>
2024-05-09 15:42:34 +0530

    added some simple demo methods for practice

--- src/main/scala/scalamethods/MethodDemo.scala
+++ src/main/scala/scalamethods/MethodDemo.scala
@@ -7,5 +7,17 @@ object MethodDemo extends App {
     a + b
   }
 
+  // with return type
+  def isBetweenA(a:Int, x:Int, y:Int): Boolean = a >= x && a <= y
+  def maxNumberA(a:Int, b:Int): Int = if (a > b) a else b
+
+  // without return type
+  def isBetweenB(a:Int, x:Int, y:Int) = a >= x && a <= y
+  def maxNumberB(a:Int, b:Int)= if (a > b) a else b
+
   println(addTwoIntegers(4, 5))
+  println(isBetweenA(3,4,5))
+  println(isBetweenB(3,4,5))
+  println(maxNumberB(3,4))
+  println(maxNumberA(3,4))
 }
